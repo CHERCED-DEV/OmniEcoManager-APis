597976b0d97e03208c3773279dea4bd5542d1d2a
Camilo Hernandez <hitmancodeme47@hotmail.com>
2024-02-07 20:03:12 -0500

    pending to manage the error

--- src/main/common/layout/services/common.service.ts
+++ src/main/common/layout/services/common.service.ts
@@ -1,18 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { Observable, forkJoin } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
 import { CultureService } from 'src/main/core/services/culture/culture.service';
 import { FooterConfig } from '../entities/footer.entity';
 import { HeaderConfig } from '../entities/header.entity';
 import { CommonConfig } from '../entities/layout.entity';
 import { FooterService } from './footer/footer.service';
 import { HeaderService } from './header/header.service';
-import { first, map, switchMap, take } from 'rxjs/operators';
-import { forkJoin, Observable } from 'rxjs';
 
 @Injectable()
 export class CommonService {
   private footerData!: FooterConfig;
   private headerData!: HeaderConfig;
-  private isFirstRequest = false;
 
   constructor(
     private cultureService: CultureService,
@@ -20,29 +19,21 @@ export class CommonService {
     private headerService: HeaderService,
   ) {}
 
-  private initializeOnFirstRequest(): void {
-    if (!this.isFirstRequest) {
-      this.cultureService
-        .cultureListener()
-        .pipe(
-          first(),
-          switchMap((culture) => {
-            console.log('culture is working: ' + culture);
-            if (culture !== null && culture !== undefined && culture !== '') {
-              return this.fetchData(culture);
-            } else {
-              return this.fetchData(null);
-            }
-          }),
-          take(1), // Se completará después de la primera emisión
-        )
-        .subscribe(({ footerData, headerData }) => {
-          this.footerData = footerData;
-          this.headerData = headerData;
-        });
-
-      this.isFirstRequest = true;
-    }
+  private initializeRequest(): void {
+    this.cultureService
+      .cultureListener()
+      .pipe(
+        switchMap((culture) => {
+          console.log('culture is working: ' + culture);
+          if (culture !== null && culture !== undefined && culture !== '') {
+            return this.fetchData(culture);
+          }
+        }),
+      )
+      .subscribe(({ footerData, headerData }) => {
+        this.footerData = footerData;
+        this.headerData = headerData;
+      });
   }
 
   private fetchData(
@@ -55,8 +46,7 @@ export class CommonService {
   }
 
   getCommonData(): CommonConfig {
-    // Llama al método de inicialización solo en la primera solicitud
-    this.initializeOnFirstRequest();
+    this.initializeRequest();
 
     const res: CommonConfig = {
       common: {
